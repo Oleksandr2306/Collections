8d9d6e5cacb23ecd4adaa6bac0d219e1c12fb00d
Oleksandr2306 <sashamelnik2306@gmail.com>
2022-09-01 11:34:29 +0300

    revision 3.0

--- Collections/Controllers/Cells/CustomCollectionViewCell.swift
+++ Collections/Controllers/Cells/CustomCollectionViewCell.swift
@@ -13,18 +13,18 @@ final class CustomCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     private enum arrayMenuOptions: String, CaseIterable {
-        case option1 = "Insert 1000 elements at the beginning of the array one-by-one"
-        case option2 = "Insert 1000 elements at the beginning of the array"
-        case option3 = "Insert 1000 elements in the middle of the array one-by-one"
-        case option4 = "Insert 1000 elements in the middle of the array"
-        case option5 = "Insert 1000 elements at the end of the array one-by-one"
-        case option6 = "Insert 1000 elements at the end of the array all at once"
-        case option7 = "Remove 1000 elements at the beginning of the array one-by-one"
-        case option8 = "Remove 1000 elements at the beginning of the array"
-        case option9 = "Remove 1000 elements in the middle of the array one-by-one"
-        case option10 = "Remove 1000 elements in the middle of the array"
-        case option11 = "Remove 1000 elements at the end of the array one-by-one"
-        case option12 = "Remove 1000 elements at the end of the array"
+        case option1 = "Insert 1000 elements at the beginning one-by-one"
+        case option2 = "Insert 1000 elements at the beginning"
+        case option3 = "Insert 1000 elements in the middle one-by-one"
+        case option4 = "Insert 1000 elements in the middle"
+        case option5 = "Insert 1000 elements at the end one-by-one"
+        case option6 = "Insert 1000 elements at the end"
+        case option7 = "Remove 1000 elements at the beginning one-by-one"
+        case option8 = "Remove 1000 elements at the beginning"
+        case option9 = "Remove 1000 elements in the middle one-by-one"
+        case option10 = "Remove 1000 elements in the middle"
+        case option11 = "Remove 1000 elements at the end one-by-one"
+        case option12 = "Remove 1000 elements at the end"
     }
     
     var isTapped = false {
@@ -32,6 +32,7 @@ final class CustomCollectionViewCell: UICollectionViewCell {
             if isTapped {
                 cellLabel.text? = ""
                 activityIndicator.isHidden = false
+                activityIndicator.startAnimating()
                 backgroundColor = .systemGray6
             } else {
                 cellLabel.isHidden = false
