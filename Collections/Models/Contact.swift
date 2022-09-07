4e2cab20fc3358b597e2a470b67e6aa27b1e7d59
Oleksandr2306 <sashamelnik2306@gmail.com>
2022-09-07 17:25:06 +0300

    Contact struct added

---
+++ Collections/Models/Contact.swift
@@ -0,0 +1,15 @@
+//
+//  Contact.swift
+//  Collections
+//
+//  Created by Oleksandr Melnyk on 22.08.2022.
+//
+
+import Foundation
+
+struct Contact {
+    var name: String
+    var phone: String
+}
+
+
